e325a6fe451b106eaf2937fedd556a8f2caf51b2
carson-sue <42878515+carson-sue@users.noreply.github.com>
2020-11-24 21:20:17 -0800

    Update main.c
    +added login feature. currently MAX_ATTEMPTS is set to 3 but can be changed
    +code is setup so that we can easily modify it once we add features for a customer.

--- main.c
+++ main.c
@@ -7,6 +7,8 @@
 #include "bank.h"
 #include "display.h"
 
+#define MAX_ATTEMPTS 3
+
 int
 main(int argc, char *argv[])
 {
@@ -15,7 +17,11 @@ main(int argc, char *argv[])
 	PGresult	*res = NULL;
 	time_t		t;
 	struct tm	tm;
+	int		attempt = 0;
 	char		c;
+	char 		userType;
+	char*		name = (char *) malloc(BUFSIZE*sizeof(char));
+	char*		pass = (char *) malloc(BUFSIZE*sizeof(char));
 
 	/* Specify the name of the database. */
 	if (argc > 1)
@@ -40,25 +46,75 @@ main(int argc, char *argv[])
 	printf("\n=========== %d-%02d-%02d %02d:%02d:%02d =========== \n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 
 	do {
-		printf("\n===========  Choose an option:  ===========\n");
-		printf("\t-a View/edit accounts\n\t-b View banks"
-			"\n\t-c View/edit customers\n\t"
-			"-t View/edit transactions\n\t-x Exit\n");
-		c = getchar();
-		getchar();
-		if (c == 'a')
-			accounts(res, conn);
-		else if (c == 'b')
-			banks(res, conn);
-		else if (c == 'c')
-			customer(res, conn);
-		else if (c == 't') {
-			transactions(res, conn);
-		}
-		else
+		if(attempt > 0)
+			printf("The username or password was incorrect. Try agian.\n");
+
+		printf("\nUsername: ");
+		scanf("%s", name);
+		printf("Password: ");
+		scanf("%s", pass);
+
+		attempt++;
+	} while(!login(name, pass, &userType, res, conn) && attempt < MAX_ATTEMPTS);
+	free(pass);
+	getchar();
+
+	if(attempt >= MAX_ATTEMPTS && userType != 'a' && userType != 'c')
+	{
+		printf("\nToo many failed attempts. Aborting...\n");
+		exit_success(conn);
+	}
+
+	printf("\nWelcome %s\n", name);
+	if(userType == 'a')
+	{
+		do {
+			printf("\n===========  Choose an option:  ===========\n");
+			printf("\t-a View/edit accounts\n\t-b View/edit banks"
+				"\n\t-c View/edit customers\n\t"
+				"-t View/edit transactions\n\t-x Exit\n");
+			c = getchar();
+			getchar();
+			if (c == 'a')
+				accounts(res, conn);
+			else if (c == 'b')
+				banks(res, conn);
+			else if (c == 'c')
+				customer(res, conn);
+			else if (c == 't') {
+				transactions(res, conn);
+			}
+			else
 			exit_success(conn);
 
-	} while (c != EOF && c != '\n' && c != 'x');
+		} while (c != EOF && c != '\n' && c != 'x');
+	}
+	else if(userType == 'c')
+	{
+		//TODO
+		do {
+			printf("\n===========  Choose an option:  ===========\n");
+			printf("\t-a View/edit accounts\n\t-b View banks"
+				"\n\t-c View/edit customers\n\t"
+				"-t View/edit transactions\n\t-x Exit\n");
+			c = getchar();
+			getchar();
+			if (c == 'a')
+				accounts(res, conn);
+			else if (c == 'b')
+				banks(res, conn);
+			else if (c == 'c')
+				customer(res, conn);
+			else if (c == 't') {
+				transactions(res, conn);
+			}
+			else
+			exit_success(conn);
+
+		} while (c != EOF && c != '\n' && c != 'x');
+	}
+	else
+		printf("Something has gone horribly wrong.\n");
 
 	exit_conn(conn);
 
